dc415faf498037cf9d1931992adff05439d8e0f3
Alina Polozhai <polozhai.alina@hotmail.com>
2024-12-19 18:28:17 +0200

    Reconfigurate webpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
@@ -41,6 +42,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
     }),
   ],
 };
