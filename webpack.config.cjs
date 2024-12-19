c91c8375b9730767b9c8607958a94980dc3ce9b6
Alina Polozhai <polozhai.alina@hotmail.com>
2024-12-19 18:48:23 +0200

    Add global variables for google console

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
@@ -46,5 +47,6 @@ module.exports = {
     new CopyPlugin({
       patterns: [{ from: "public", to: "." }],
     }),
+    new Dotenv(),
   ],
 };
